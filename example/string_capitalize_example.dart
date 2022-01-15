09c8f352d23b28921a1f1a4b9cdf8b9debb916d7
Leonardo Rosa <leonardoorosaa16@gmail.com>
2022-01-15 16:20:25 -0300

    added example

--- example/string_capitalize_example.dart
+++ example/string_capitalize_example.dart
@@ -1,1 +1,10 @@
+import 'dart:io';
+import 'package:string_capitalize/string_capitalize.dart';
 
+void main() {
+  print('Type your name?');
+
+  final name = stdin.readLineSync() ?? '';
+
+  print(name.capitalizeOrFail());
+}
